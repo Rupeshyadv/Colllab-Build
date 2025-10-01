eca6fde2bc6f4f3002d405b2f95ee90078756ef3
Your Name <rupeshyadav9978@gmail.com>
2025-10-01 16:12:43 +0530

    fix: fixed join room issue by adding roomDest

--- frontend/src/components/Room/JoinRoom.jsx
+++ frontend/src/components/Room/JoinRoom.jsx
@@ -3,14 +3,15 @@ import { useNavigate } from 'react-router-dom';
 import { joinSessionRoom } from '../../api/sessionRoomApi';
 
 const JoinRoom = () => {
-  const [roomId, setRoomId] = useState('')
+  const [roomDest, setRoomDest] = useState('')  // sesssionId/language 
 
   const navigate = useNavigate()  
   const handleJoinByRoomId = async () => {
+    const [roomId, language] = roomDest.split('/')
     if (roomId.trim()) {
         const data = await joinSessionRoom(roomId)
 
-        navigate(`/room/${roomId}`)
+        navigate(`/room/${roomDest}`)
     }
   };
 
@@ -23,8 +24,8 @@ const JoinRoom = () => {
                     <input
                         type="text"
                         placeholder="Enter room id"
-                        value={roomId}
-                        onChange={(e) => setRoomId(e.target.value)}
+                        value={roomDest}
+                        onChange={(e) => setRoomDest(e.target.value)}
                         className="px-4 py-2 bg-gray-800 border border-gray-700 rounded-lg text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                     />
                     <button
